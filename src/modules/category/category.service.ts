b269918d43b217f38184b46b8f3cba44ae083d2f
Виталий Иванов <vitalkaivanov009@mail.ru>
2024-08-17 13:11:31 +0300

    комментарии к коду

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -16,16 +16,19 @@ export class CategoryService {
      * @return Полученная категория
      */
     async getCategory(id: string, slug: string): Promise<ICategory> {
+        // Получаем категорию из БД
         const category: ICategory | undefined = await this.dbService.getCategory({
             where: {
                 OR: [{ id: id }, { slug: slug }],
             },
         });
 
+        // Проверяем существует ли категория
         if (!category) {
             throw new HttpException('Категория не найдена', HttpStatus.NOT_FOUND);
         }
 
+        // Возвращаем категорию
         return category;
     }
 
@@ -35,16 +38,19 @@ export class CategoryService {
      * @return Созданная категория
      */
     async createCategory(body: CreateCategoryDto): Promise<ICategory> {
+        // Получаем категорию из БД по slug
         const category: ICategory | undefined = await this.dbService.getCategory({
             where: {
                 slug: body.slug,
             },
         });
 
+        // Проверяем существует ли категория с таким slug
         if (category) {
             throw new HttpException('Категория с таким уникальным названием уже существует', HttpStatus.CONFLICT);
         }
 
+        // Создаем категорию в БД и возвращаем результат
         return await this.dbService.createCategory({ data: { id: crypto.randomUUID(), ...body } });
     }
 
@@ -54,18 +60,23 @@ export class CategoryService {
      * @return Обновленная категория
      */
     async updateCategory(body: UpdateCategoryDto): Promise<ICategory> {
+        // Деструктурируем данные
         const { id: idCategory, ...dataForUpdate } = body;
 
+        // Проверяем есть ли хотя бы одно поле для обновления категории
         if (!Object.keys(dataForUpdate).some((item) => item !== undefined)) {
             throw new HttpException('Для обновления необходимо изменить хотя бы одно поле', HttpStatus.BAD_REQUEST);
         }
 
+        // Получаем категорию из БД
         const category: ICategory | undefined = await this.dbService.getCategory({ where: { id: idCategory } });
 
+        // Проверяем существует ли категория в БД
         if (!category) {
             throw new HttpException('Категория с таким id не найдена', HttpStatus.NOT_FOUND);
         }
 
+        // Обновляем категорию и возвращаем результат
         return await this.dbService.updateCategory({ where: { id: idCategory }, data: dataForUpdate });
     }
 
@@ -75,16 +86,19 @@ export class CategoryService {
      * @return Удаленная категория
      */
     async deleteCategory(id: string): Promise<ICategory> {
+        // Получаем категорию из БД по id
         const category: ICategory | undefined = await this.dbService.getCategory({
             where: {
                 id: id,
             },
         });
 
+        // Проверяем существует ли категория
         if (!category) {
             throw new HttpException('Категория с таким идентификатором не найдена', HttpStatus.NOT_FOUND);
         }
 
+        // Удаляем категорию и возвращаем результат
         return await this.dbService.deleteCategory({
             where: {
                 id: id,
